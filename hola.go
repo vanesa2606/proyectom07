eeefe6942d2a5249e8f95a594f083eb34058b252
vanesa <ramonorregomarquez@gmail.com>
2019-01-08 11:46:24 +0100

    primer commit

---
+++ hola.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	/*fmt.Printf("***** mi programa con go *******\n")
+
+	var edad int = 18
+
+	if edad > 18 {
+		fmt.Printf("Eres mayor de edad")
+	} else {
+		fmt.Printf("Eres menor de edad")
+	}*/
+
+	for i := 1; i < 15; i++ {
+		fmt.Println(i, "es el número actual")
+	}
+}
